f67ae5e2f6faed3791ae534ade495d5568264985
s298410 <s298410@10.20.1.182>
2024-10-23 13:52:47 -0400

    f

--- src/sandbox/Methods.java
+++ src/sandbox/Methods.java
@@ -24,7 +24,14 @@ public class Methods {
 		jtf.setText(hardCodedMessage);
 	}
 	
-	
+	// DESCRIPTION - Displays hard-coded message onto textField passed in as parameter.
+	// PARAMETERS - JTextField jtf
+	// RETURN TYPE - void
+	public static void rowanMethod(JTextField jtf)  {
+			
+		String hardCodedMessage = "i paused my game to write this";
+		jtf.setText(hardCodedMessage);
+	}
 	
 	
 }
