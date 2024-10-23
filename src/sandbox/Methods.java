00f469c392badb36bb2c3698bc068efba0b1f785
Mr. Wong <77798339+LW-OCSB@users.noreply.github.com>
2024-10-23 13:49:14 -0400

    Add files via upload

---
+++ src/sandbox/Methods.java
@@ -0,0 +1,30 @@
+package sandbox;
+
+import javax.swing.JTextField;
+
+public class Methods {
+
+	/*
+	// DESCRIPTION - Displays hard-coded message onto textField passed in as parameter.
+	// PARAMETERS - JTextField jtf
+	// RETURN TYPE - void
+	public static void <student>Method(JTextField jtf)  {
+		
+		String hardCodedMessage = "";
+		jtf.setText(hardCodedMessage);
+	}
+	*/
+	
+	// DESCRIPTION - Displays hard-coded message onto textField passed in as parameter.
+	// PARAMETERS - JTextField jtf
+	// RETURN TYPE - void
+	public static void mrWongMethod(JTextField jtf)  {
+		
+		String hardCodedMessage = "Mr. Wong";
+		jtf.setText(hardCodedMessage);
+	}
+	
+	
+	
+	
+}
