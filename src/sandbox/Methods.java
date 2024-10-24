701151b840dcf5062e8877900adc99eed3468cc4
s463536 <s463536@10.20.1.174>
2024-10-24 13:09:24 -0400

    2

--- src/sandbox/Methods.java
+++ src/sandbox/Methods.java
@@ -49,5 +49,14 @@ public class Methods {
 	}
 	
 	
+	// DESCRIPTION - Displays hard-coded message onto textField passed in as parameter.
+	// PARAMETERS - JTextField jtf
+	// RETURN TYPE - void
+	public static void muhammadMethod(JTextField jtf)  {
+		
+		String hardCodedMessage = "lilonick257 <3";
+		jtf.setText(hardCodedMessage);
+	}
+	
 	
 }
