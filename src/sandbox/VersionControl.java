ac90ca3a94176cf36b2f88343f9b40ccc0f98a8d
s298418 <s298418@10.20.165.232>
2024-10-24 13:15:29 -0400

    e

--- src/sandbox/VersionControl.java
+++ src/sandbox/VersionControl.java
@@ -232,7 +232,7 @@ public class VersionControl {
 		coleButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				// classMethods.<student>Method(textField);
+				classMethods.coleMethod(textField);
 			}
 		});
 		coleButton.setFont(new Font("Tahoma", Font.BOLD, 11));
